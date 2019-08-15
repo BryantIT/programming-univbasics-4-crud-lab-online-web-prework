f146bb2627aff137bca05a850c4712fca543f26a
Bryant Richards <dragonseyeimages@gmail.com>
2019-08-15 22:44:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,37 +1,37 @@
 def create_an_empty_array 
-  []
+  array=[]
 end
 
 def create_an_array
- a=["I", "am", "really", "learning"]
+ array=["I", "am", "really", "learning"]
 end
 
 def add_element_to_end_of_array(array, element)
-  a<< "arrays!"
+  array<< "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-   a.unshift "wow"
+   array.unshift "wow"
 end
 
 def remove_element_from_end_of_array(array)
-  a.pop
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  a.shift
+  array.shift
 end
   
 def retrieve_element_from_index(array, index_number)
-  
+  array[1]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0}]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
